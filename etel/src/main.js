98d790c1031fef6b57b777e1315e9c82c2c7556b
Zonda <74469252+ragsimul@users.noreply.github.com>
2023-12-10 12:45:03 +0900

    가격 업그레이드

--- etel/src/main.js
+++ etel/src/main.js
@@ -1,12 +1,23 @@
 var etel_dust = document.getElementById('etel_price');
-var etel_stone = document.getElementById('etel_stone_full_price');
+var etel_stone = document.getElementById('etel_stone_price');
 
 etel_dust.addEventListener('input', updateResult);
 
 function updateResult() {
     var etel_dust = document.getElementById('etel_price');
-    var etel_stone = document.getElementById('etel_stone_full_price');
+    var etel_stone = document.getElementById('etel_stone_price');
+    var etel_aqua = document.getElementById('etel_aqua_price');
+    var etel_topaz = document.getElementById('etel_topaz_price');
+    var etel_amethyst = document.getElementById('etel_amethyst_price');
+    var etel_amber = document.getElementById('etel_amber_price');
 
     var etel_stone_price = etel_dust.value;
     etel_stone.innerText = etel_stone_price * 5 + 100000;
+
+    etel_aqua.innerText = (etel_stone.innerText * 5)*3 + 100000*5
+    etel_topaz.innerText = (etel_stone.innerText * 5)*6 + 200000*5
+    etel_amethyst.innerText = (etel_stone.innerText * 5)*10 + 300000*5
+    etel_amber.innerText = (etel_stone.innerText * 5)*15 + 500000*5
+    
+
 }
