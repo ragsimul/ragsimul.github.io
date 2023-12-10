2f6781a04ddeaab1087eba4854a3de3135c05f9c
Zonda <74469252+ragsimul@users.noreply.github.com>
2023-12-10 12:54:17 +0900

    다시

--- etel/src/main.js
+++ etel/src/main.js
@@ -5,14 +5,15 @@ etel_dust.addEventListener('input', updateResult);
 
 function updateResult() {
     var etel_dust = document.getElementById('etel_price');
-    var etel_stone = document.getElementById('etel_stone_price');
+    var etel_stone_price = document.getElementById('etel_stone_price');
     var etel_aqua = document.getElementById('etel_aqua_price');
     var etel_topaz = document.getElementById('etel_topaz_price');
     var etel_amethyst = document.getElementById('etel_amethyst_price');
     var etel_amber = document.getElementById('etel_amber_price');
 
-    var etel_stone_price = etel_dust.value;
-    etel_stone.innerText = (etel_stone_price * 5 + 100000).toLocaleString();
+    var etel_price = etel_dust.value;
+    var etel_stone = etel_price * 5 + 100000;
+    etel_stone_price.innerText = (etel_stone).toLocaleString();
 
     etel_aqua.innerText = ((etel_stone.innerText * 5)*3 + 100000*5).toLocaleString();
     etel_topaz.innerText = ((etel_stone.innerText * 5)*6 + 200000*5).toLocaleString();
